c443f37472b8cb2fec889db099ffa5fa96175e8b
supremgy <supremgy@icloud.com>
2024-01-31 02:25:19 +0900

    return 값 type 지정하기

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   //test
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(8080);
 }
 bootstrap();
