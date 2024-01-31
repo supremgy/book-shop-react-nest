f6a15e9ff9f159b460fa4d0f10be1be003f284e0
supremgy <supremgy@icloud.com>
2024-01-31 21:44:03 +0900

    book 모듈 개발중

--- server/src/book/book.controller.ts
+++ server/src/book/book.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
+import { BookAllDto } from './dto/book-all.dto';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get('/')
+  getBooks(@Query(ValidationPipe) bookAllDto: BookAllDto) {
+    // return this.bookService.getBooks(bookAllDto);
+  }
+}
