94ba5a828b5e12f502d0a5088d7b562d218211d0
supremgy <supremgy@icloud.com>
2024-02-17 11:45:35 +0900

    책 전체조회 api 구현

--- server/src/book/book.service.ts
+++ server/src/book/book.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Like } from 'src/like/like.entity';
-import { LikeRepository } from 'src/like/like.repository';
-import { SelectQueryBuilder } from 'typeorm';
-import { Book } from './book.entity';
 import { BookRepository } from './book.repository';
 import { BookAllDto } from './dto/book-all.dto';
 
@@ -12,17 +8,15 @@ export class BookService {
   constructor(
     @InjectRepository(BookRepository)
     private bookRepository: BookRepository,
-    @InjectRepository(LikeRepository)
-    public likeRepository: LikeRepository,
   ) {}
-  async getBooks(bookAllDto: BookAllDto): Promise<Object> {
-    const { categoryId, news } = bookAllDto;
+  async getBooks(bookAllDto: BookAllDto): Promise<object> {
+    // const { categoryId, news } = bookAllDto;
 
     //카테고리별 신상조회
-    if (categoryId && news) {
-      return this.bookRepository.latestBooksByCategory(bookAllDto);
-      //카테고리별 전체조회
-    }
+    // if (categoryId && news) {
+    //   return this.bookRepository.latestBooksByCategory(bookAllDto);
+    //   //카테고리별 전체조회
+    // }
     // else if (category_id) {
     //   return this.bookRepository.booksByCategory(bookAllDto);
     //   //신상 전체조회
